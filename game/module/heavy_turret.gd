b517d17a61c878063f9479fa4ee57724074d5a9a
Baptiste THEPAUT <Devend460>
2026-05-29 13:14:20 -0400

    Mesh de la tourelle heavy

--- game/module/heavy_turret.gd
+++ game/module/heavy_turret.gd
@@ -9,3 +9,12 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func shoot() -> void:
+	super.shoot()
+	if (target != null):
+		var direction = target.global_position - $SpawnPoint.global_position
+		look_at($SpawnPoint.global_position + direction)
+		rotation.x = 0
+		rotation.z = 0
