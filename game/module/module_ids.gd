dbe3ea82cf0405b724671e071be8c07f0b03480e
Matéu Mirade <mateu@pellaptop.org>
2026-06-04 21:52:33 +0200

    First UI Buttons done

--- game/module/module_ids.gd
+++ game/module/module_ids.gd
@@ -3,6 +3,7 @@ class_name ModuleId
 
 enum Of {
 	TURRET,
+	MISSILE_LAUNCHER,
 	TUBE,
 	HATCH,
 	AUTO_MINER,
--- game/ui/building_ui.gd
+++ game/ui/building_ui.gd
@@ -1,8 +1,20 @@
 extends Control
 class_name BuildingUI
 
-signal on_module_requested(id : ModuleId)
+signal on_module_requested(id : ModuleId.Of)
 
-func on_button_pressed(id : ModuleId) -> void:
+func on_button_pressed(id : ModuleId.Of) -> void:
 	# Vérifications dans les ressouces du joueur
 	on_module_requested.emit(id);
+
+
+func _turret_button_pressed() -> void:
+	on_button_pressed(ModuleId.Of.TURRET);
+
+
+func _missile_launcher_button_pressed() -> void:
+	on_button_pressed(ModuleId.Of.MISSILE_LAUNCHER);
+
+
+func _tube_button_pressed() -> void:
+	on_button_pressed(ModuleId.Of.TUBE);
