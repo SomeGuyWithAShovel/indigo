cb0ccac8e3edceee00ac04a9ccc1c8d95b2b24f5
Pelle <mariahtobias.pelle@gmail.com>
2026-06-06 17:29:34 -0400

    Commit du build de vendredi corrigé (j'avais pas push)

--- game/ui/indicators/quota_container.gd
+++ game/ui/indicators/quota_container.gd
@@ -4,8 +4,3 @@ class_name QuotaContainer
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	super._ready();
-	DayNightSystem.quota_changed.connect(on_quota_change);
-
-func on_quota_change(new_quota : int) -> void:
-	bar.max_value = new_quota;
-	bar.value = 0;
