19ff76885a90bdd35f167ea0c1325214dfa31620
Paul <194802520+pavilleneu@users.noreply.github.com>
2026-06-11 12:41:51 -0400

    damaged signal on health component

--- game/components/health.gd
+++ game/components/health.gd
@@ -12,14 +12,22 @@ signal died(from : HealthComponent); # On pourra rajouter des paramètres si bes
 signal invincibility_started(from : HealthComponent);
 signal invincibility_ended(from : HealthComponent);
 
-var _current_health: int:
+signal on_damaged(new_hp: int, old_hp: int); # I didn't want to update already existing signals, so I created my own.
+# Because there weren't any way to know when we are damaged (health_changed doesn't tell what was the previous health so we can't compare)
+
+var _current_health: int :
 	get:
 		return _current_health;
 	set(value):
+		if (value < _current_health) :
+			on_damaged.emit(value, _current_health);
+			pass;
 		_current_health = value;
 		health_changed.emit(self, _current_health);
 		if _current_health <= 0:
 			died.emit(self);
+			pass;
+		return;
 
 func get_health() -> int:
 	return _current_health;
